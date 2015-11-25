61feda07da8fc3daca85547f7d969b0ff3b07906
Chris Scott <cyberstrike@gmail.com>
2015-11-25 16:55:16 -0500

    added drag and drop to add file

--- app/assets/javascripts/cases.js
+++ app/assets/javascripts/cases.js
@@ -4,6 +4,8 @@
 
 $(document).on('ready page:load', function() {
 
+  window.debug = true;
+
   // Tabs
 
   $('.tab-content div').click(function (e) {
@@ -16,7 +18,6 @@ $(document).on('ready page:load', function() {
   var canvas = this.__canvas = new fabric.Canvas('c');
   fabric.Object.prototype.transparentCorners = false;
 
-  //var $ = function(id){return document.getElementById(id);};
 
   var rect = new fabric.Rect({
     width: 100,
@@ -27,7 +28,7 @@ $(document).on('ready page:load', function() {
   });
 
   canvas.add(rect).setActiveObject(rect);
-  var text = new fabric.Text('Hello.. nurse', { left: 100, top: 100 });
+  var text = new fabric.Text('Hello.. Nurse!', { left: 100, top: 100 });
 
   canvas.add(text);
   fabric.Image.fromURL('data:image/gif;base64,R0lGODlhEAAQAMQAAORHHOVSKudfOulrSOp3WOyDZu6QdvCchPGolfO0o/XBs/fNwfjZ0frl3/zy7////wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAkAABAALAAAAAAQABAAAAVVICSOZGlCQAosJ6mu7fiyZeKqNKToQGDsM8hBADgUXoGAiqhSvp5QAnQKGIgUhwFUYLCVDFCrKUE1lBavAViFIDlTImbKC5Gm2hB0SlBCBMQiB0UjIQA7', function(oImg) {
@@ -36,9 +37,38 @@ $(document).on('ready page:load', function() {
 
 
   // Text Input
+  //
 
-  var $textInput = $('#TextInput');
+  var $textInput = $('#textInput');
+  var $textAdd = $('#textAdd');
 
+  // Add text from input
+
+  $textAdd.on('click', function() {
+
+        // Get text from input
+        var text = $textInput.val();
+
+        // Create Fabric Text Object
+        var fabricText = new fabric.Text( text, {
+          left: fabric.util.getRandomInt(0, 200),
+          top: fabric.util.getRandomInt(0, 400),
+          fontFamily: 'helvetica',
+          angle: 0,
+          fill: '#000000',
+          scaleX: 0.5,
+          scaleY: 0.5,
+          fontWeight: '',
+          hasRotatingPoint: true
+        });
+
+        // Add Fabric Text Object to Canvas
+        canvas.add(fabricText);
+
+      }
+  );
+
+  // Set the current value of text object to the input
   $textInput.keyup(function(){
     var activeObject = canvas.getActiveObject();
     if (activeObject && activeObject.type === 'text') {
@@ -49,13 +79,65 @@ $(document).on('ready page:load', function() {
 
 
   //
+  // File Drop
+  //
+
+  $droparea = $('.drop-area');
+
+
+  // Stop Defaults on the drop area to prevent page redirection
+
+  $droparea.on({
+    'dragover': stopEvent,
+    'dragleave': stopEvent
+  });
+
+  // Listen to drop event
+  $droparea.on('drop', function(e){
+    stopEvent(e);
+    dropFile(e)
+  });
+
+  // Grab file from drop event then add to canvas
+  function dropFile(e){
+
+    var file = "";
+
+    if (e.originalEvent.dataTransfer != null){
+      file = URL.createObjectURL(e.originalEvent.dataTransfer.files[0]);
+      if (e.target.files != null){
+        file = URL.createObjectURL(e.target.files[0]);
+      }
+    }
+
+    fabric.Image.fromURL(file,
+        function(oImg){canvas.add(oImg)},
+        {
+          left: fabric.util.getRandomInt(0, 200),
+          top: fabric.util.getRandomInt(0, 400),
+          angle: 0,
+          hasRotatingPoint: true
+        }
+    );
+
+  }
+
+
+
+
+
+
+  //
   // Advanced Controls
   //
+
+  // TODO: Convert the advanced controls to jQuery
 
   // Angle
   var angleControl = $('#angle-control');
 
   angleControl.on('change', function () {
+
         var activeObject = canvas.getActiveObject();
         activeObject.setAngle(parseInt(this.value, 10)).setCoords();
         canvas.renderAll();
@@ -69,6 +151,7 @@ $(document).on('ready page:load', function() {
     canvas.renderAll();
   };
 
+  // Scale Input
   var scaleValue = $('scaleValue');
   scaleValue.onchange = function(){
     scaleControl.value = parseFloat(this.value);
@@ -76,20 +159,31 @@ $(document).on('ready page:load', function() {
     canvas.renderAll();
   };
 
+  // Top Control
   var topControl = $('top-control');
   topControl.onchange = function() {
     rect.setTop(parseInt(this.value, 10)).setCoords();
     canvas.renderAll();
   };
 
+  // Left Control
   var leftControl = $('left-control');
   leftControl.onchange = function() {
     rect.setLeft(parseInt(this.value, 10)).setCoords();
     canvas.renderAll();
   };
 
+  //
+  // Utilities
+  //
+
+  // Mirror values to controls and inputs
   function updateControls() {
     var activeObject = canvas.getActiveObject();
+
+    $textInput.val(activeObject.text);
+
+    // Advanced Controls
     scaleControl.value = activeObject.getScaleX();
     scaleValue.value = parseFloat(activeObject.getScaleX());
     angleControl.val(
@@ -97,6 +191,8 @@ $(document).on('ready page:load', function() {
     leftControl.value = rect.getLeft();
     topControl.value = rect.getTop();
   }
+
+  // Pass values from canvas events
   canvas.on({
     'object:selected': updateControls,
     'object:moving': updateControls,
@@ -104,4 +200,20 @@ $(document).on('ready page:load', function() {
     'object:resizing': updateControls,
     'object:rotating': updateControls
   });
+
+  // Simple logger
+  function debug (theArgs){
+    if (window.debug === true) {
+      console.log(theArgs);
+    }
+  }
+
+  // Stops Event Propagation
+
+  function stopEvent (e){
+    e.preventDefault();
+    e.stopPropagation();
+    debug([e])
+  }
+
 });
