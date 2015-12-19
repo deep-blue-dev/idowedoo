3a9655674dbd8481b7876b5be87c5259c8fb85b5
johnq <johnq@johnqs-MacBook-Air.local>
2015-12-19 15:11:54 -0500

    cart_row and shopping cart view done

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,7 +2,7 @@ class CartsController < ApplicationController
   def show
     @order_items = current_order.order_items
     p "1111111111"
-    p @order_items.size
+    p @order_items
     p "1111111111"
     @total = current_order # notes TODO need make a total amount to pass it to stripe go to _shopping_cart !!
   end
