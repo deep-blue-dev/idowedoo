09d959cc5c3aaf698d7568c0e967ec1d67ce7f17
johnq <johnq@johnqs-MacBook-Air.local>
2015-12-29 13:02:35 -0500

    shipment price computed now need to create view to input address that case's will be shiped #3

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -6,11 +6,11 @@ class ChargesController < ApplicationController
   # Data used for stripe
   before_action :set_current_order, only: [:charge]
 
-  def new
-  end
+    def new
+    end
 
-  def index
-  end
+    def index
+    end
 
     def shipping
       p "Easy Post started"
@@ -44,7 +44,7 @@ class ChargesController < ApplicationController
       :from_address => fromAddress,
       :parcel => parcel
       )
-      
+
       shipment.rates.each do |rate|
         puts (rate.carrier)
         puts (rate.service)
@@ -74,10 +74,9 @@ class ChargesController < ApplicationController
       p "*********************************"
       redirect_to charges_path
 
-    # rescue Stripe::CardError => e
-    #   redirect_to products_path
-
-  end
+    rescue Stripe::CardError => e
+      redirect_to products_path
+    end
 
   private
 
@@ -86,31 +85,3 @@ class ChargesController < ApplicationController
   end
 
 end
-
-
-
-
-
-# NOTE example
-# @order_items = current_order.order_items
-# # notes TODO need make a total amount to pass it to stripe go to _shopping_cart !!
-#  @subtotal = current_order.subtotal
-#  @subtotal_tax = current_order.tax
-#  @order_total = @subtotal + @subtotal_tax
-
-# def charge
-#   @subtotal = current_order.subtotal.to_f
-#   @subtotal_tax = current_order.tax.to_f
-#   @order_total = @subtotal + @subtotal_tax
-#   p "Subtotal:"
-#   p @subtotal
-#   p "Subtotal Tax"
-#   p @subtotal_tax
-#   p "Stripe.api_key:"
-#   p Stripe.api_key
-#   p "****************************** Charge begun"
-#   p "Order Total:"
-#   p @order_total
-#   redirect_to cart_path
-#   p "****************************** Charge ended"
-# end
