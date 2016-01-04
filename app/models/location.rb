e33fa50777bc838cac3204afaa7a44d8bbd3eddb
Volney Tejeda <volneytejeda10@gmail.com>
2016-01-04 13:31:53 -0500

    Add user title to location form
    value set to default if nil

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,11 +2,15 @@ class Location < ActiveRecord::Base
   #gems
   #scopes
   #callbacks
+  before_save :default_title
   #relations
   belongs_to :order
   belongs_to :user
   #validations
 
   #methods
-
+  private
+    def default_title
+      self.user_title = "Default" if self.user_title == ''
+    end
 end
