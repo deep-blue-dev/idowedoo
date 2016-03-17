3d3bd93cae161c2f4ece12e914bd21afac0d2f3b
Peter Richards <peter.jrichards@yahoo.com>
2016-03-18 05:59:53 +0700

    passing page req spec

--- spec/requests/cases_request_spec.rb
+++ spec/requests/cases_request_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "Cases", type: :request do
-  describe "index" do
-    it 'returns 200' do
+  describe "routes work" do
+    it 'new' do
       get new_case_path
       expect(response.status).to eq(200)
     end
