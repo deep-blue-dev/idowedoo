4079219b74277b2987d8a1758ae46b316b428053
Peter Richards <peter.jrichards@yahoo.com>
2016-03-23 12:29:11 +0700

    cases controller...incomplete

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,7 +1,43 @@
 class CasesController < ApplicationController
 
+  def index
+    if current_user.present?
+      @cases = current_user.cases
+    else
+      @cases = []
+    end
+    @new_case = Case.new
+  end
+
+  def create
+    if current_user.present?
+      kase = current_user.cases.build(case_params)
+      if kase.save
+        flash[:success] = t("cases_controller.create.success")
+        redirect_to edit_case_path(kase)
+      else
+        flash[:error] = t("cases_controller.create.error")
+        # render :back
+        redirect_to :index
+      end
+    else
+      ## Allow unauthed users to design case, but they must register to save
+    end
+  end
+
   def new
     @cases = Case.all
     @order_item = OrderItem.new
+  end
+
+  def edit
+    @cases = Case.all
+    @order_item = OrderItem.new
+  end
+
+  private 
+
+  def case_params
+    params.require(:case).permit(:title)
   end
 end
