eac9be9d70217f4340c8586ba9c4f54b08c3caf7
Peter Richards <peter.jrichards@yahoo.com>
2016-03-31 16:21:11 +0700

    fixed page 2 of campaign creation

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -27,7 +27,19 @@ class Campaign < ActiveRecord::Base
 
   include ActionView::Helpers::DateHelper
 
+  
+  ## CONSTANTS
+
+  # This is the cost price per case (before any profit), the sale value cannot go below this
+  # At some point this may become dynamic based on phone type, # of colors, number sold, etc.
   MIN_BASE_PRICE = 10.00
+  # This is the price we think a customer should sell their case at, it gives them and us a profit.
+  RECOMMENDED_BASE_PRICE = 15.00
+
+  STARTING_GOAL_VALUE = 20
+
+  
+  ## ASSOCIATIONS
 
   belongs_to :user
 
@@ -36,6 +48,14 @@ class Campaign < ActiveRecord::Base
   has_many :campaign_case_options, dependent: :destroy
   accepts_nested_attributes_for :campaign_case_options
 
+  
+  ## VALIDATIONS
+
+  validates_numericality_of :base_price, minimum: MIN_BASE_PRICE,
+    too_short: "This base price is below cost, please set it to a minimum of #{MIN_BASE_PRICE}"
+
+
+  ## INSTANCE METHODS
 
   def length
     distance_of_time_in_words(self.start, self.finish)
