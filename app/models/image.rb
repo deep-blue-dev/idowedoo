e1d504b91e9ba70ed78dbfb4f05044f316597d7f
Peter Richards <peter.jrichards@yahoo.com>
2016-04-01 16:23:06 +0700

    annotate image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: images
+#
+#  id                 :integer          not null, primary key
+#  filename           :string
+#  case_id            :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#
+
 class Image < ActiveRecord::Base
   belongs_to :case
 
