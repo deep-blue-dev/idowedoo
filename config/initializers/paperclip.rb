7b6b4238c7fb2f93c5949d721b953b5bf274c3ca
Peter Richards <peter.jrichards@yahoo.com>
2016-04-07 20:32:53 +0700

    s3 init

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,2 @@
-# Paperclip::Attachment.default_options[:url] = 'idowedo.s3-us-west-1.amazonaws.com'
-# Paperclip::Attachment.default_options[:path] = ':rails_root/public/system/:class/:attachment/:id_partition/:style/:filename'
-
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
