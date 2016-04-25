25bc4aba12653f259ea83dd88e3630d2d53f260c
Peter Richards <peter.jrichards@yahoo.com>
2016-04-26 03:41:11 +0700

    removed commented code and prints

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,27 +2,19 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params.merge(user: current_user))
-
-    # stripe_customer_params = {token: order.stripe_token, email: @order.email}
-    # stripe_customer_params.merge!(current_user_id: @order.user.id) if @order.user
+    
     if @order.save
       if @order.calculate_and_charge
         flash[:success] = "Thanks you for ordering! Here is your order information."
-        puts "\n\n\n 1 \n\n\n"
         redirect_to order_path(id: @order.token) and return
-        
-        # total_price = order_params[:num_items] * order_params[:]
-        # campaign_extra = {
-        #   total_price: 
-        # }
       else
+        flash[:danger] = "There was an issue charging your card. Please try again."
         # Issue with charging, must be invalid Stripe info
-        puts "\n\n\n 2 \n\n\n"
         redirect_to new_order_path(id: @order.id) and return
       end
     else
+      flash[:danger] = "Some form fields were missing. Please try again."
       # Issue with validation, must be missing attrs
-      puts "\n\n\n 3 \n\n\n"
       redirect_to new_order_path(id: @order.id) and return
     end
   end
